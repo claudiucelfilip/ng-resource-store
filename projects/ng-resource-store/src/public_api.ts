9e95123a0371adfc077e9e843393a852116a9813
Claudiu Teodor Filip <cfilip@opswat.com>
2018-10-07 10:54:02 +0300

    - remove unneded files

--- projects/ng-resource-store/src/public_api.ts
+++ projects/ng-resource-store/src/public_api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ng-resource-store
  */
 
-export * from './lib/ng-resource-store.service';
 export * from './lib/ng-resource-store.module';
 export * from './lib/ng-resource-store.directive';
